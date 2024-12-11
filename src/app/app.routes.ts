55918e371538fddb0ce879c8c06f896ddd1d9613
qcmarcel <qc.marcel@gmail.com>
2024-12-11 13:24:48 -0500

    Add Bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {ProfileComponent} from './components/profile/profile.component';
+import {HomeComponent} from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'profile/:id', component: ProfileComponent},
+  {path: '**', pathMatch: 'full', redirectTo: ''}
+];
